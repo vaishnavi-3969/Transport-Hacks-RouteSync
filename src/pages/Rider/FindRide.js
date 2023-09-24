d997e36ce66aa2587a1c47f35663dd4958d1f140
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-09-24 13:06:23 +0530

    find a ride

--- src/pages/Rider/FindRide.js
+++ src/pages/Rider/FindRide.js
@@ -1,9 +1,198 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  Box,
+  Button,
+  Divider,
+  Input,
+  Select,
+  Stack,
+  Text,
+  useColorMode,
+} from '@chakra-ui/react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import Confetti from 'react-confetti';
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton } from "@chakra-ui/react"; // Import Chakra UI modal components
+
+const containerStyle = {
+  width: '100%',
+  height: '500px',
+};
+
+const center = {
+  lat: 37.7749,
+  lng: -122.4194,
+};
 
 const FindRide = () => {
-  return (
-    <div>FindRide</div>
-  )
-}
+  const [markers, setMarkers] = useState([]);
+  const [filter, setFilter] = useState('');
+  const [selectedRide, setSelectedRide] = useState(null);
+  const [showConfetti, setShowConfetti] = useState(false);
+  const [isBookingModalOpen, setIsBookingModalOpen] = useState(false);
 
-export default FindRide
+  const addMarker = (event) => {
+    const newMarker = {
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng(),
+    };
+    setMarkers([...markers, newMarker]);
+  };
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const handleRideClick = (ride) => {
+    setSelectedRide(ride);
+  };
+
+  const handleBookRide = () => {
+    setShowConfetti(true);
+    setIsBookingModalOpen(true);
+  };
+
+  const handleCloseConfirmation = () => {
+    setShowConfetti(false);
+    setIsBookingModalOpen(false);
+  };
+
+  return (
+   <>
+     <Box p={4}>
+      <h1>Find a Ride</h1>
+      <LoadScript googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY}>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={12}
+          onClick={addMarker}
+        >
+          {markers.map((marker, index) => (
+            <Marker key={index} position={marker} />
+          ))}
+        </GoogleMap>
+      </LoadScript>
+      <Box mt={4}>
+        <Text fontSize="lg" mb={2}>
+          Filter Rides:
+        </Text>
+        <Stack direction="row" spacing={4}>
+          <Select
+            placeholder="Select a filter"
+            value={filter}
+            onChange={handleFilterChange}
+          >
+            <option value="all">All Rides</option>
+            <option value="car">Cars</option>
+            <option value="bike">Bikes</option>
+            <option value="ev">Electric Vehicles</option>
+          </Select>
+          <Input
+            placeholder="Search by location..."
+          />
+          <Button colorScheme="teal">Apply</Button>
+        </Stack>
+      </Box>
+      <Box mt={4}>
+        <Text fontSize="lg" mb={2}>
+          Available Rides:
+        </Text>
+        {selectedRide ? (
+          <Box borderWidth="1px" p={4} rounded="md" shadow="md" bg="gray.50">
+            <Text fontSize="lg">{selectedRide.title}</Text>
+            <Text fontSize="md" color="gray.500">
+              Destination: {selectedRide.destination}
+            </Text>
+            <Text fontSize="md" color="gray.500">
+              Date: {selectedRide.date}
+            </Text>
+            <Button
+              colorScheme="teal"
+              size="sm"
+              onClick={handleBookRide}
+            >
+              Book Ride
+            </Button>
+            <Divider />
+            <Button
+              colorScheme="teal"
+              size="sm"
+              onClick={() => setSelectedRide(null)}
+            >
+              Close
+            </Button>
+          </Box>
+        ) : (
+          <Stack spacing={4}>
+            {[
+              {
+                title: 'Ride to Work',
+                destination: 'Office',
+                date: 'Oct 15, 2023',
+                type: 'car',
+                price: 200, 
+              },
+              {
+                title: 'City Bike Ride',
+                destination: 'Park',
+                date: 'Oct 17, 2023',
+                type: 'bike',
+                price: 50, 
+              },
+              {
+                title: 'Eco-Friendly Commute',
+                destination: 'Downtown',
+                date: 'Oct 20, 2023',
+                type: 'ev',
+                price: 150, 
+              },
+            ]
+              .filter((ride) => filter === 'all' || ride.type === filter)
+              .map((ride, index) => (
+                <Box
+                  key={index}
+                  borderWidth="1px"
+                  p={4}
+                  rounded="md"
+                  shadow="md"
+                  bg="gray.50"
+                  onClick={() => handleRideClick(ride)}
+                  cursor="pointer"
+                >
+                  <Text fontSize="lg">{ride.title}</Text>
+                  <Text fontSize="md" color="gray.500">
+                    Destination: {ride.destination}
+                  </Text>
+                  <Text fontSize="md" color="gray.500">
+                    Date: {ride.date}
+                  </Text>
+                  <Text fontSize="md" color="gray.500">
+                    Price: Rs {ride.price}
+                  </Text>
+                </Box>
+              ))}
+          </Stack>
+        )}
+      </Box>
+      {showConfetti && <Confetti />}
+      <Modal isOpen={isBookingModalOpen} onClose={handleCloseConfirmation}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Booking Confirmation</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Congratulations! Your ride has been booked successfully.
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="teal" onClick={handleCloseConfirmation}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Box>
+   </>
+  );
+};
+
+export default FindRide;
--- src/pages/Rider/RentAVehicle.js
+++ src/pages/Rider/RentAVehicle.js
@@ -1,9 +1,109 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Text,
+  useColorMode,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+} from '@chakra-ui/react';
 
-const RentAVehicle = () => {
+function RentAVehicle() {
+  const { colorMode } = useColorMode();
+  const [formData, setFormData] = useState({
+    vehicleType: 'car',
+    pickupLocation: '',
+    pickupDate: '',
+    returnDate: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form Data:', formData);
+  };
+
   return (
-    <div>RentAVehicle</div>
-  )
+    <Flex
+      direction="column"
+      align="center"
+      minH="calc(100vh - 100px)"
+      bg={colorMode === 'light' ? 'gray.100' : 'gray.900'}
+      padding={{ base: 4, md: 10 }}
+    >
+      <Heading as="h1" size="xl" my={6} color="teal.500">
+        Rent a Vehicle
+      </Heading>
+
+      <Box maxW="xl" p={6} rounded="lg" shadow="lg" bg="white" w="100%">
+        <Text fontSize="lg" mb={6}>
+          Choose your preferred vehicle type and start your journey.
+        </Text>
+
+        <form onSubmit={handleSubmit}>
+          <FormControl mb={4}>
+            <FormLabel>Vehicle Type</FormLabel>
+            <Select
+              name="vehicleType"
+              value={formData.vehicleType}
+              onChange={handleInputChange}
+            >
+              <option value="car">Car</option>
+              <option value="bike">Bike</option>
+              <option value="ev">Electric Vehicle (EV)</option>
+            </Select>
+          </FormControl>
+
+          <FormControl mb={4}>
+            <FormLabel>Pickup Location</FormLabel>
+            <Input
+              type="text"
+              name="pickupLocation"
+              value={formData.pickupLocation}
+              onChange={handleInputChange}
+            />
+          </FormControl>
+
+          <FormControl mb={4}>
+            <FormLabel>Pickup Date</FormLabel>
+            <Input
+              type="date"
+              name="pickupDate"
+              value={formData.pickupDate}
+              onChange={handleInputChange}
+            />
+          </FormControl>
+
+          {formData.vehicleType === 'car' && (
+            <FormControl mb={4}>
+              <FormLabel>Return Date</FormLabel>
+              <Input
+                type="date"
+                name="returnDate"
+                value={formData.returnDate}
+                onChange={handleInputChange}
+              />
+            </FormControl>
+          )}
+
+          <Button type="submit" colorScheme="teal" size="lg">
+            {formData.vehicleType === 'car' ? 'Book a Car' : 'Rent'}
+          </Button>
+        </form>
+      </Box>
+    </Flex>
+  );
 }
 
-export default RentAVehicle
+export default RentAVehicle;
