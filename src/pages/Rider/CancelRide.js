657c44a4cabc6b69ac9f72f9dc43550157f09e13
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-09-24 14:34:14 +0530

    updated

--- src/pages/Rider/CancelRide.js
+++ src/pages/Rider/CancelRide.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  Box,
+  Button,
+  Heading,
+  Text,
+  useColorMode,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 const CancelRide = () => {
-  return (
-    <div>CancelRide</div>
-  )
-}
+  const { colorMode } = useColorMode();
+  const [rideCancelled, setRideCancelled] = useState(false);
 
-export default CancelRide
+  function handleCancel() {
+    setRideCancelled(true);
+  }
+
+  return (
+    <Box
+      p={4}
+      bg={colorMode === 'light' ? 'gray.100' : 'gray.900'}
+      minH="calc(100vh - 100px)"
+      textAlign="center"
+    >
+      <Heading as="h1" size="xl" my={6} color="teal.500">
+        Cancel Ride
+      </Heading>
+
+      <Box maxW="xl" p={6} rounded="lg" shadow="lg" bg="white" mx="auto">
+        {!rideCancelled ? (
+          <>
+            <Text fontSize="lg" mb={6}>
+              Are you sure you want to cancel this ride?
+            </Text>
+
+            <Button colorScheme="teal" size="lg" width="100%" onClick={handleCancel}>
+              Confirm Cancellation
+            </Button>
+          </>
+        ) : (
+          <>
+            <Text fontSize="lg" mb={6}>
+              Ride Cancelled
+            </Text>
+
+            <Link to="/rider-home">
+              <Button colorScheme="teal" size="lg" width="100%">
+                Back to Rider Home
+              </Button>
+            </Link>
+          </>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default CancelRide;
--- src/pages/Rider/RiderRewards.js
+++ src/pages/Rider/RiderRewards.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from 'react';
+import {
+  Box,
+  Heading,
+  SimpleGrid,
+  Text,
+  useColorMode,
+  Icon,
+} from '@chakra-ui/react';
+import { FaMedal, FaStar, FaTrophy } from 'react-icons/fa';
 
 const RiderRewards = () => {
-  return (
-    <div>RiderRewards</div>
-  )
-}
+  const { colorMode } = useColorMode();
+  
+  // dummy rewards data
+  const rewards = [
+    {
+      title: 'Eco-Warrior Badge',
+      description: 'You\'ve saved 100 kg of CO2 emissions!',
+      icon: FaMedal,
+    },
+    {
+      title: 'Super Saver',
+      description: 'You\'ve taken 50 rides and counting!',
+      icon: FaStar,
+    },
+    {
+      title: 'Champion Rider',
+      description: 'You\'re the top rider of the month!',
+      icon: FaTrophy,
+    },
+  ];
 
-export default RiderRewards
+  return (
+    <Box
+      p={4}
+      bg={colorMode === 'light' ? 'gray.100' : 'gray.900'}
+      minH="calc(100vh - 100px)"
+      textAlign="center"
+    >
+      <Heading as="h1" size="xl" my={6} color="teal.500">
+        Your Rewards 
+      </Heading>
+
+      <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={6}>
+        {rewards.map((reward, index) => (
+          <Box
+            key={index}
+            p={6}
+            rounded="lg"
+            shadow="lg"
+            bg="white"
+            textAlign="center"
+          >
+            <Icon as={reward.icon} w={10} h={10} color="teal.500" />
+            <Heading as="h2" size="lg" my={4} color="teal.500">
+              {reward.title}
+            </Heading>
+            <Text fontSize="md" color="gray.500">
+              {reward.description}
+            </Text>
+          </Box>
+        ))}
+      </SimpleGrid>
+    </Box>
+  );
+};
+
+export default RiderRewards;
