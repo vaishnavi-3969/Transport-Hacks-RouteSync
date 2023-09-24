24a4a27d2e33cfb982a3329e3b231ae5f78c3ad9
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-09-24 15:39:23 +0530

    updated

--- src/pages/Driver/DriverHome.js
+++ src/pages/Driver/DriverHome.js
@@ -1,9 +1,132 @@
-import React from 'react'
+import React from 'react';
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  SimpleGrid,
+  Stack,
+  Text,
+  useColorMode,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { FaMapMarker, FaCar, FaBell } from 'react-icons/fa';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-const DriverHome = () => {
+const mapContainerStyle = {
+  width: '100%',
+  height: '300px',
+};
+
+function DriverHome() {
+  const { colorMode } = useColorMode();
+
   return (
-    <div>DriverHome</div>
-  )
+    <Flex
+      direction="column"
+      align="center"
+      minH="calc(100vh - 100px)"
+      bg={colorMode === 'light' ? 'gray.100' : 'gray.900'}
+      padding={{ base: 4, md: 10 }}
+    >
+      <Heading as="h1" size="xl" my={6} color="teal.500">
+        Welcome, Driver!
+      </Heading>
+
+      <Box maxW="xl" p={6} rounded="lg" shadow="lg" bg="white" w="100%">
+        <Stack spacing={4} mb={6}>
+          <Text fontSize="lg">
+            Ready to start driving and earn rewards? Get on the road now!
+          </Text>
+        </Stack>
+
+        <Heading as="h2" size="lg" my={4} color="teal.500">
+          Your Upcoming Trips
+        </Heading>
+
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4}>
+          <TripCard
+            title="Morning Commute"
+            destination="Office"
+            date="Oct 15, 2023"
+          />
+          <TripCard
+            title="Evening Pickup"
+            destination="Airport"
+            date="Oct 20, 2023"
+          />
+        </SimpleGrid>
+      </Box>
+
+      <LoadScript googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY}>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          center={{ lat: 37.7749, lng: -122.4194 }}
+          zoom={12}
+        >
+        </GoogleMap>
+      </LoadScript>
+
+      <Link to="/driver-profile" style={{ textDecoration: 'none' }}>
+        <Button mt={6} colorScheme="teal" size="lg">
+          View Profile
+        </Button>
+      </Link>
+
+      <Link to="/notifications" style={{ textDecoration: 'none' }}>
+        <Button
+          mt={4}
+          variant="outline"
+          colorScheme="teal"
+          leftIcon={<FaBell />}
+        >
+          Notifications
+        </Button>
+      </Link>
+
+      <Link to="/create-pool" style={{ textDecoration: 'none' }}>
+        <Button mt={4} colorScheme="teal" size="lg">
+          Create New Pool
+        </Button>
+      </Link>
+
+      <Link to="/subscribe-pool" style={{ textDecoration: 'none' }}>
+        <Button mt={4} colorScheme="teal" size="lg">
+          Put your Car/Bike/Cab on subscription
+        </Button>
+      </Link>
+    </Flex>
+  );
 }
 
-export default DriverHome
+function TripCard({ title, destination, date }) {
+  return (
+    <Box
+      borderWidth="1px"
+      rounded="md"
+      p={4}
+      shadow="md"
+      bg="gray.50"
+      textAlign="center"
+    >
+      <Text fontSize="lg">{title}</Text>
+      <Text fontSize="md" color="gray.500">
+        <FaMapMarker style={{ marginRight: '0.2em' }} /> Destination: {destination}
+      </Text>
+      <Text fontSize="md" color="gray.500">
+        Date: {date}
+      </Text>
+      <Button
+        colorScheme="teal"
+        size="sm"
+        width="100%"
+        as={Link}
+        to="/cancel-trip"
+      >
+        Cancel
+      </Button>
+    </Box>
+  );
+}
+
+export default DriverHome;
--- src/pages/Rider/RiderNotifications.js
+++ src/pages/Rider/RiderNotifications.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import {
+  Box,
+  Heading,
+  Text,
+  useColorMode,
+  VStack,
+} from '@chakra-ui/react';
+
+import notificationSound from '../../assets/notifications.mp3';
 
 const RiderNotifications = () => {
-  return (
-    <div>RiderNotifications</div>
-  )
-}
+  const { colorMode } = useColorMode();
+  const [notifications, setNotifications] = useState([]);
 
-export default RiderNotifications
+  useEffect(() => {
+    const notificationMessages = [
+      'Ride request confirmed',
+      'New reward unlocked',
+      'Upcoming ride reminder',
+      'You earned 50 points',
+    ];
+
+    const interval = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * notificationMessages.length);
+      const randomMessage = notificationMessages[randomIndex];
+
+      const audio = new Audio(notificationSound);
+      audio.play();
+
+      setNotifications([...notifications, randomMessage]);
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [notifications]);
+
+  return (
+    <Box
+      p={4}
+      bg={colorMode === 'light' ? 'gray.100' : 'gray.900'}
+      minH="calc(100vh - 100px)"
+      textAlign="center"
+    >
+      <Heading as="h1" size="xl" my={6} color="teal.500">
+        Rider Notifications
+      </Heading>
+
+      <VStack align="stretch" spacing={4}>
+        {notifications.map((notification, index) => (
+          <Box
+            key={index}
+            p={4}
+            rounded="lg"
+            shadow="lg"
+            bg="white"
+            textAlign="center"
+          >
+            <Text fontSize="lg">{notification}</Text>
+          </Box>
+        ))}
+      </VStack>
+    </Box>
+  );
+};
+
+export default RiderNotifications;
